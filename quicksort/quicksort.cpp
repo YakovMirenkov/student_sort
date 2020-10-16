0b8e2ee417affd0cf3debf68dc790b43b72612d7
Yakov Mirenkov <Yakov.Mirenkov@student.msu.ru>
2020-10-16 14:12:12 +0300

    quicksort: fix copying students

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -3,18 +3,16 @@
 void quicksort(student *a, int n)
 {
 	int i, j;
-	student tmp;
 
 	while( n>1 )
 	{
 		i = 0;
 		j = n - 1;
-		tmp = a[n/2];
 		while( i<j )
 		{
-			while( tmp>a[i] )
+			while( a[n/2]>a[i] )
 				i++;
-			while( a[j]>tmp )
+			while( a[j]>a[n/2] )
 				j--;
 			// Найдено первое a[i]>=a[n/2] и последнее a[j]<=a[n/2]
 
